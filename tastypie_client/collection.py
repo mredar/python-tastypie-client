b2feba9bf7531c9ddbcedf70acea1b729c1ca2a5
Mark Redar <mredar@gmail.com>
2013-05-16 16:43:06 -0700

    Implemented collection as an iterator. Now instead of using a function get_all() to traverse the collections object, you can simply use a for loop. Also handles when there are more objects than will be presented in a single page -- iteraters using the meta "next" url. Also, added a .meta to the collection for the current metadata from tastypie.

--- tastypie_client/collection.py
+++ tastypie_client/collection.py
@@ -9,12 +9,39 @@ class Collection(object):
     def __init__(self, url, schema_url = None):
         self.url = url
         self.schema_url = schema_url
+        self.meta = { 'limit':None, 'offset':None, 'next':None, 'previous':None, 'total_count':None}
+        self.url_next = self.url
 
-    def get_all(self):
-        response, content = Http().request(self.url)
-        for obj in json.loads(content)['objects']:
-            yield Resource(url = urljoin(self.url, obj['resource_uri']), fields = obj)
-        
+    @staticmethod
+    def __load_url(url):
+        '''Load a url into a dictionary: meta & a list of objects
+        '''
+        response, content = Http().request(url)
+        meta = json.loads(content)['meta']
+        objects = [obj for obj in json.loads(content)['objects']]
+        return meta, objects
+
+    def __iter__(self):
+        '''Get the next set of Resources for the collection'''
+        while True:
+            self.meta, objects = self.__load_url(self.url_next)
+            for obj in objects:
+                yield Resource(url = urljoin(self.url, obj['resource_uri']), fields = obj)
+            if not self.meta['next']:
+                raise StopIteration
+            self.url_next = self.url+self.meta['next']
+
+    def __len__(self):
+        if not self.meta['total_count']:
+            self.meta, objs = self.__parse_url(self.url)
+        return self.meta['total_count']
+
+    def __contains__(self, v):
+        return True if self.get(v) else False
+
+    def __getitem__(self, v):
+        return self.get(v)
+
     def get(self, id):
         return Resource(urljoin(self.url, id))
         
